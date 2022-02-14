41244105dc66a476683a534dd30c3d320d369fb5
Chamsol Yoon <dantert@gmail.com>
2022-02-14 22:07:52 +0900

    스프링 컨테이너와 스프링 빈
    스프링 컨테이너 생성
    컨테이너에 등록된 모든 빈 조회
    스프링 빈 조회 - 기본

---
+++ src/test/java/io/hkarling/core/bean/ApplicationContextBasicFindTest.java
@@ -0,0 +1,49 @@
+package io.hkarling.core.bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import io.hkarling.core.AppConfig;
+import io.hkarling.core.member.MemberService;
+import io.hkarling.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    
+    @Test
+    @DisplayName("이름 없이 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() { // 좋은 케이스는 아니다.
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+//        ac.getBean("xxxxx", MemberService.class);
+//        MemberService xxxxx = ac.getBean("xxxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+            () -> ac.getBean("xxxxx", MemberService.class));
+    }
+
+}
--- src/test/java/io/hkarling/core/bean/SpringFrameworkBasicApplicationTests.java
+++ src/test/java/io/hkarling/core/bean/SpringFrameworkBasicApplicationTests.java
@@ -1,4 +1,4 @@
-package io.hkarling.core;
+package io.hkarling.core.bean;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
