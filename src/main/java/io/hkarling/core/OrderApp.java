a0329947250fccc40556c37b4f4ac9f0cc25feee
Chamsol Yoon <dantert@gmail.com>
2022-02-14 21:10:19 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용 3
    OrderApp 동작 확인

--- src/main/java/io/hkarling/core/OrderApp.java
+++ src/main/java/io/hkarling/core/OrderApp.java
@@ -11,15 +11,16 @@ import io.hkarling.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-       // MemberService memberService = new MemberServiceImpl(null);
-        //OrderService orderService = new OrderServiceImpl(null);
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member memberA = new Member(memberId, "memberA", Grade.VIP);
-//        memberAemberService.join(memberA);
+        memberService.join(memberA);
 
-//        Order order = orderService.createOrder(memberId, "itemA", 10000);
-//        System.out.println("order = " + order);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
     }
 
 }
