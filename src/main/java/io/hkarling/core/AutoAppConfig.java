dda97e32f26be95bfc20345f633c1ded4c480d9a
Chamsol Yoon <dantert@gmail.com>
2022-02-19 23:29:31 +0900

    컴포넌트 스캔 2
    탐색 위치와 기본 스캔 대상

--- src/main/java/io/hkarling/core/AutoAppConfig.java
+++ src/main/java/io/hkarling/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//    basePackages = "io.hkarling.core.member", // 탐색 패키지 시작, 기본은 설정 클래스의 위치 하위. 따라서 최상단에 만든다
     excludeFilters = @Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
     // 예제코드의 유지를 위해 @Configuration 제외. 보통은 하지 않는다
 )
