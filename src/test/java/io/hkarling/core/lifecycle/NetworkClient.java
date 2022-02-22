9c02d7ddf7137287fabdab75153e65ad2adc8f70
Chamsol Yoon <dantert@gmail.com>
2022-02-22 20:34:56 +0900

    빈 생명주기 콜백
    인터페이스 InitializingBean, DisposableBean

--- src/test/java/io/hkarling/core/lifecycle/NetworkClient.java
+++ src/test/java/io/hkarling/core/lifecycle/NetworkClient.java
@@ -1,13 +1,20 @@
 package io.hkarling.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+//        connect();
+//        call("초기화 연결 메시지");
+
+        /* 생성자 호출, url = null
+         * connect:null
+         * call:null message:초기화 연결 메시지 */
     }
 
     public void setUrl(String url) {
@@ -27,4 +34,22 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close:" + url);
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+
+    /**
+     * 초기화, 소멸 인터페이스 단점
+     *  - 스프링 전용 인터페이스에 의존한다
+     *  - 초기화, 소멸 메서드의 이름 변경할 수 없다
+     *  - 내가 코드를 고칠 수 없는 외부 라이브러리에 적용 불가
+     */
 }
