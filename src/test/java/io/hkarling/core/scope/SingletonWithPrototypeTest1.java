0952754f0ae1569402280d62fb3d88ccfa98562c
Chamsol Yoon <dantert@gmail.com>
2022-02-23 20:37:48 +0900

    빈 스코프
    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 해결 | Provider

--- src/test/java/io/hkarling/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/io/hkarling/core/scope/SingletonWithPrototypeTest1.java
@@ -4,7 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -36,23 +40,50 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = context.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2); // ClientBean 내부 prototypeBean 은 clientBean 싱글턴이 생성될 때 만들어서 주입되었다
+//        assertThat(count2).isEqualTo(2); // ClientBean 내부 prototypeBean 은 clientBean 싱글턴이 생성될 때 만들어서 주입되었다
+        assertThat(count1).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
 
-        private final PrototypeBean prototypeBean;
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
-        ClientBean(PrototypeBean prototypeBean) {
-            System.out.println("ClientBean.ClientBean");
-            this.prototypeBean = prototypeBean;
-        }
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        private ApplicationContext ac;
+
+//        private final PrototypeBean prototypeBean;
+//
+//        ClientBean(PrototypeBean prototypeBean) {
+//            System.out.println("ClientBean.ClientBean");
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
+        /* Dependency Lookup(DL) 의존관계 탐색
+         * - 의존관계를 외부에서 주입(DI)받는게 아니라 직접 필요한 의존관계를 찾는 것
+         * - 이런 식으로 어플리케이션 컨텍스트 전체를 주입받게 되면, 스프링 컨테이너에 종속적인 코드가 되고, 단위 테스트도 어려워진다
+         * - ObjectProvider 를 사용한다 (ObjectFactory 에서 편이기능 추가)
+         *
+         * ObjectProvider
+         * - 스프링 컨테이너를 통해 빈을 찾아서 반환한다(DL)
+         * - 스프링이 제공하는 기능을 사용하지만, 기능이 단순하므로 단위테스트를 만들거나 mock 코드를 만들기 쉬워진다
+         *
+         * javax.inject.Provider
+         * - JSR-330 Provider: 자바 표준
+         * - javax.inject:javax.inject:1 라이브러리를 gradle 에 추가해야 한다
+         * - get() 메서드 하나로 기능이 매우 단순하며, 표준이므로 다른 컨테이너에서도 사용할 수 있다
+         */
     }
 
     @Scope("prototype")
@@ -70,7 +101,7 @@ public class SingletonWithPrototypeTest1 {
 
         @PostConstruct
         public void init() {
-            System.out.println("PrototypeBean.init");
+            System.out.println("PrototypeBean.init " + this);
         }
 
         @PreDestroy
