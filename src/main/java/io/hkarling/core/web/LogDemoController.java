110a9fe5239c5b42cd50f32cd47db2405ab2d8aa
Chamsol Yoon <dantert@gmail.com>
2022-02-24 20:24:28 +0900

    빈 스코프
    스코프와 Provider

--- src/main/java/io/hkarling/core/web/LogDemoController.java
+++ src/main/java/io/hkarling/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package io.hkarling.core.web;
 import io.hkarling.core.common.MyLogger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,14 +13,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+//    private final MyLogger myLogger; // request 스코프 빈을 어플리케이션 로딩 시점에 요청. 오류남
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
-        String requestURL = request.getRequestURI();
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
